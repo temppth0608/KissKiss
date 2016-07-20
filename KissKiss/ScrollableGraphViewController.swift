e8b6e9bb0592479f73b8973ab5e3fb3c81ebb131
seolheelee <rain00723@gmail.com>
2016-07-21 02:40:44 +0900

    ScrollableGrapViewController에 예시 추가(추후개정), 쓰지 않는 그래프종류 소스 지움

---
+++ KissKiss/ScrollableGraphViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ScrollableGraphViewController.swift
+//  KissKiss
+//
+//  Created by 이설희 on 2016. 7. 17..
+//  Copyright © 2016년 mashup. All rights reserved.
+//
+
+import UIKit
+
+class ScrollableGraphViewController: UIViewController {
+    
+    var graphView = ScrollableGraphView()
+    var graphConstraints = [NSLayoutConstraint]()
+    
+    var label = UILabel()
+    var labelConstraints = [NSLayoutConstraint]()
+    
+    // Data
+    let numberOfDataItems = 29
+    
+    lazy var data: [Double] = self.generateRandomData(self.numberOfDataItems, max: 50)
+    lazy var labels: [String] = self.generateSequentialLabels(self.numberOfDataItems, text: "FEB")
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        graphView = ScrollableGraphView(frame: self.view.frame)
+        graphView = createDarkGraph(self.view.frame)
+        
+        graphView.setData(data, withLabels: labels)
+        self.view.addSubview(graphView)
+        
+        setupConstraints()
+        
+    }
+    
+    private func createDarkGraph(frame: CGRect) -> ScrollableGraphView {
+        let graphView = ScrollableGraphView(frame: frame)
+        
+        graphView.backgroundFillColor = UIColor.colorFromHex("#333333")
+        
+        graphView.lineWidth = 1
+        graphView.lineColor = UIColor.colorFromHex("#777777")
+        graphView.lineStyle = ScrollableGraphViewLineStyle.Smooth
+        
+        graphView.shouldFill = true
+        graphView.fillType = ScrollableGraphViewFillType.Gradient
+        graphView.fillColor = UIColor.colorFromHex("#555555")
+        graphView.fillGradientType = ScrollableGraphViewGradientType.Linear
+        graphView.fillGradientStartColor = UIColor.colorFromHex("#555555")
+        graphView.fillGradientEndColor = UIColor.colorFromHex("#444444")
+        
+        graphView.dataPointSpacing = 80
+        graphView.dataPointSize = 2
+        graphView.dataPointFillColor = UIColor.whiteColor()
+        
+        graphView.referenceLineLabelFont = UIFont.boldSystemFontOfSize(8)
+        graphView.referenceLineColor = UIColor.whiteColor().colorWithAlphaComponent(0.2)
+        graphView.referenceLineLabelColor = UIColor.whiteColor()
+        graphView.numberOfIntermediateReferenceLines = 5
+        graphView.dataPointLabelColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        
+        graphView.shouldAnimateOnStartup = true
+        graphView.shouldAdaptRange = true
+        graphView.adaptAnimationType = ScrollableGraphViewAnimationType.Elastic
+        graphView.animationDuration = 1.5
+        graphView.rangeMax = 50
+        graphView.shouldRangeAlwaysStartAtZero = true
+        
+        return graphView
+    }
+    
+    private func setupConstraints() {
+        
+        self.graphView.translatesAutoresizingMaskIntoConstraints = false
+        graphConstraints.removeAll()
+        
+        let topConstraint = NSLayoutConstraint(item: self.graphView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0)
+        let rightConstraint = NSLayoutConstraint(item: self.graphView, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: self.graphView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0)
+        let leftConstraint = NSLayoutConstraint(item: self.graphView, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 0)
+        
+        graphConstraints.append(topConstraint)
+        graphConstraints.append(bottomConstraint)
+        graphConstraints.append(leftConstraint)
+        graphConstraints.append(rightConstraint)
+        
+        self.view.addConstraints(graphConstraints)
+    }
+    
+    
+    // Data Generation
+    private func generateRandomData(numberOfItems: Int, max: Double) -> [Double] {
+        var data = [Double]()
+        for _ in 0 ..< numberOfItems {
+            var randomNumber = Double(random()) % max
+            
+            if(random() % 100 < 10) {
+                randomNumber *= 3
+            }
+            
+            data.append(randomNumber)
+        }
+        return data
+    }
+    
+    private func generateSequentialLabels(numberOfItems: Int, text: String) -> [String] {
+        var labels = [String]()
+        for i in 0 ..< numberOfItems {
+            labels.append("\(text) \(i+1)")
+        }
+        return labels
+    }
+    
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    
+
+}
